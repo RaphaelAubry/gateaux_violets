bee4469a2eba81c8041622e6d154ca56fd78c2ba
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-01-03 15:16:44 +0100

    2 digits

--- app/models/line.rb
+++ app/models/line.rb
@@ -2,5 +2,11 @@ class Line < ApplicationRecord
   belongs_to :basket
   belongs_to :cake
 
+  include ActionView::Helpers::NumberHelper
+
   QUANTITY = (1..10)
+
+   def total_2d
+    number_with_precision(total, precision: 2)
+  end
 end
