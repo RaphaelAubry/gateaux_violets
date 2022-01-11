a0c6d7d39bdea08fa561a1701350185d4f04225f
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-01-11 20:02:35 +0100

    basket icon

--- app/helpers/baskets_helper.rb
+++ app/helpers/baskets_helper.rb
@@ -1,3 +1,10 @@
 module BasketsHelper
-
+  def mybasket
+    basket = Basket.where(user_id: current_user.id).last
+      if basket.status == Basket::STATUS[3]
+          basket = Basket.new(user_id: current_user.id, status: Basket::STATUS[0])
+          basket.save
+      end
+      return basket_path(basket)
+  end
 end
