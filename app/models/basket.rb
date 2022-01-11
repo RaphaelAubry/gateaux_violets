29e9278e14daee46164cc2001bd24ada2a81e8d9
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-01-11 17:18:09 +0100

    totalize method

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -7,4 +7,12 @@ class Basket < ApplicationRecord
   PAYMENT_TYPE = ["Bank transfer", "Cash"]
 
   IBAN = "FR76 1234 5647 9191 58"
+
+  def totalize
+    total = 0
+    self.lines.each do |line|
+      total += line.total
+    end
+    return total
+  end
 end
