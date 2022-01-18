d18462e40364cb8a91b84c42e6eed548bac328ec
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-01-18 15:49:29 +0100

    pages footer

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'cakes#index', as: :home
   get 'baskets/:id/invoice', to: 'baskets#invoice', as: :invoice
+  get 'pages/identification', to: 'pages#identification', as: :identification
+  get 'pages/contact', to: 'pages#contact', as: :contact
+  get 'pages/delivery', to: 'pages#delivery', as: :delivery
+  get 'pages/sales_legal', to: 'pages#sales_legal', as: :sales_legal
 
   devise_for :users
 
