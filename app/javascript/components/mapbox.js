3131566ead6fabb2474409ce853304b2d138c893
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-01-27 15:12:28 +0100

    css

--- app/javascript/components/mapbox.js
+++ app/javascript/components/mapbox.js
@@ -16,6 +16,16 @@ const initMapBox = () => {
     });
 
     map.addControl(new mapboxgl.FullscreenControl());
+    map.addControl(new mapboxgl.GeolocateControl({
+        positionOptions: {
+          enableHighAccuracy: true
+        },
+        // When active the map will receive updates to the device's location as it changes.
+        trackUserLocation: true,
+        // Draw an arrow next to the location dot to indicate which direction the device is heading.
+        showUserHeading: true
+      })
+    );
 
     const marker = new mapboxgl.Marker({
       color: '#3A1678'
