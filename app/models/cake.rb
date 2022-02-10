a684a96c3a4e410fa651cedb4446eca609ebef67
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-02-10 11:23:39 +0100

    I18n

--- app/models/cake.rb
+++ app/models/cake.rb
@@ -1,6 +1,7 @@
 class Cake < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
+  validates :size, presence: true, numericality: { other_than: 0 }
 
   has_many :lines
 
