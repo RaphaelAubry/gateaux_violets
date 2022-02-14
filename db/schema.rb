6068bf0c0cb5f08e4c699bc7df72cc9102ef091e
RaphaelAubry <raphael.aubry@hotmail.fr>
2022-02-14 13:09:41 +0100

    cdg

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_02_131351) do
+ActiveRecord::Schema.define(version: 2022_02_14_114322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 2022_02_02_131351) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "status"
     t.string "payment_type"
+    t.boolean "sales_rules"
     t.index ["user_id"], name: "index_baskets_on_user_id"
   end
 
